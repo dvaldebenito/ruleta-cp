4a01033ef656480f058d4053dfe89d5903c25d2c
Daniel Valdebenito <dvaldebenito.externo@coopeuch.cl>
2020-01-09 16:03:04 -0300

    prueba

---
+++ precache-manifest.bccff2f94c8b8cb1f8c3e20318e51c66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95be533e25949a3f7579208da973b958",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d5c1a4091554c756e63",
+    "url": "/static/css/main.24aeb873.chunk.css"
+  },
+  {
+    "revision": "30da033a47775dabd548",
+    "url": "/static/js/2.353e7fd8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.353e7fd8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d5c1a4091554c756e63",
+    "url": "/static/js/main.b63b3413.chunk.js"
+  },
+  {
+    "revision": "0fbcc7732065a023ae6d",
+    "url": "/static/js/runtime-main.6f87d395.js"
+  }
+]);
